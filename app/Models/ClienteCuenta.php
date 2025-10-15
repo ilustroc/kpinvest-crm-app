3ec9e8bf3fe190bb350c8ea324409f94759a56f3
ilustroc <62394589+ilustroc@users.noreply.github.com>
2025-10-15 10:10:53 -0500

    Realizamos cambios en controller CargaClientes

--- app/Models/ClienteCuenta.php
+++ app/Models/ClienteCuenta.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class ClienteCuenta extends Model
 {
     protected $table = 'clientes_cuentas';
-    protected $guarded = []; // permitir asignación masiva
+    protected $guarded = [];
 }
